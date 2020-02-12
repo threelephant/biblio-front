c3f58a7613cb4e58a236869474f9bb697f063557
threelephant <threelephant@gmail.com>
2020-02-13 02:59:11 +0700

    Update Authentication page
    * Added button 'Log In'
    * Added image
    * Added background color

--- src/elements/textfields.js
+++ src/elements/textfields.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles'
+import React from 'react'
+import TextField from '@material-ui/core/TextField'
+import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
+import { makeStyles, withTheme } from '@material-ui/core/styles'
 
 const useStyles = makeStyles(theme => ({
   auth: {
-    padding: '6em 3em 0 0',
+    margin: '6em 3em 0 0',
+    float: 'right',
+    flexGrow: 1,
+  },
+
+  textfield: {
+    width: '28em',
+    backgroundColor: 'white',
+  },
+
+  h1: {
+    margin: '0 13.6em 0 0',
+    fontFamily: 'sans-serif',
+  },
+
+  button: {
+    margin: '1.5em 23.3em 0 0',
+  },
+
+  image: {
+    margin: '2em 0 0 2.5em',
   },
 }))
 
@@ -13,36 +34,55 @@ export default function () {
   const classes = useStyles()
 
   return (
-    <div className={classes.auth}>
-      <Grid
-        container
-        direction="column"
-        justify="space-evenly"
-        alignItems="flex-end"
-        spacing={2}
-        >
-          <Grid key={0} item>
-            <h1>Authentication</h1>
-          </Grid>
-          <Grid key={1} item>
-            <TextField  
-                style={{ margin: 8 }}
-                id="login" 
-                type="login"
-                label="Login" 
-                variant="outlined"
-              />
-          </Grid>
-          <Grid key={2} item>  
-            <TextField
-                id="standard-password-input"
-                label="Password"
-                type="password"
-                autoComplete="current-password"
-                variant="outlined"
-              />    
+    <>
+      <img 
+        src={require('../images/book_girl.png')} 
+        alt="Girl with a book"
+        className={classes.image}>
+      </img>
+      <div className={classes.auth}>
+        <Grid
+          container
+          direction="column"
+          justify="flex-end"
+          alignItems="flex-end"
+          spacing={2}
+          >
+            <Grid key={0} item className={classes.h1}>
+              <h1>Authentication</h1>
             </Grid>
-    </Grid>
-    </div>
+            <Grid key={1} item>
+              <TextField  
+                  id="login" 
+                  type="login"
+                  label="Login" 
+                  variant="outlined"
+                  className={classes.textfield}
+                />
+            </Grid>
+            <Grid key={2} item>  
+              <TextField
+                  id="standard-password-input"
+                  label="Password"
+                  type="password"
+                  autoComplete="current-password"
+                  variant="outlined"
+                  className={classes.textfield}
+                />    
+            </Grid>
+            <Grid 
+              key={3} 
+              item variant="contained">
+              <Button 
+                variant="contained"
+                size="large" 
+                className={classes.button} 
+                color="primary">
+                Log In
+              </Button>
+            </Grid>
+        </Grid>
+      </div>
+    </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TextFields from './elements/textfields.js';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import TextFields from './elements/textfields.js'
+import './index.css'
 
 function App() {
   return (
     <TextFields />
-  );
+  )
 }
 
-ReactDOM.render(<App />, document.querySelector('#app'));
+ReactDOM.render(<App />, document.querySelector('#app'))
