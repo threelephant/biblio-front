e224bc8188eac1eb8ba3529ac4c5c0224d4c53d7
threelephant <threelephant@gmail.com>
2020-02-12 23:33:51 +0700

    Added componets
    * Added login textfield
    * Added password textfield

--- src/elements/textfields.js
+++ src/elements/textfields.js
@@ -1,31 +1,48 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
-import GridList from '@material-ui/core/GridList'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  auth: {
+    padding: '6em 3em 0 0',
+  },
+}))
 
 export default function () {
+  const classes = useStyles()
+
   return (
-    <Grid
-      container
-      direction="column"
-      justify="center"
-      alignItems="flex-end"
-      >
-      <GridList padding={10}>
-        <TextField 
-            id="login" 
-            type="login"
-            label="Login" 
-            variant="outlined"
-          />
-        <TextField
-            id="standard-password-input"
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-            variant="outlined"
-          />                        
-      </GridList>
+    <div className={classes.auth}>
+      <Grid
+        container
+        direction="column"
+        justify="space-evenly"
+        alignItems="flex-end"
+        spacing={2}
+        >
+          <Grid key={0} item>
+            <h1>Authentication</h1>
+          </Grid>
+          <Grid key={1} item>
+            <TextField  
+                style={{ margin: 8 }}
+                id="login" 
+                type="login"
+                label="Login" 
+                variant="outlined"
+              />
+          </Grid>
+          <Grid key={2} item>  
+            <TextField
+                id="standard-password-input"
+                label="Password"
+                type="password"
+                autoComplete="current-password"
+                variant="outlined"
+              />    
+            </Grid>
     </Grid>
+    </div>
   );
 }
