b04bb0103a5878abfdf8f70fbe967870d07909ed
threelephant <threelephant@gmail.com>
2020-03-05 10:56:29 +0700

    Almost complete book page
    * Added divider
    * Added "Also bought" menu
    * More stylish

---
+++ src/objects/book.js
@@ -0,0 +1,23 @@
+const book = {
+  bookId: -1,
+  title: "",
+  author: "",
+  price: -1,
+  pageNumber: -1,
+  editor: "",
+  cover: "",
+  weight: -1,
+  genre: "",
+  publisher: "",
+  series: "",
+  translator: "",
+  language: "",
+  date: -1,
+  height: -1,
+  width: -1,
+  length: -1,
+  rating: -1,
+  url: ""
+}
+
+export default book
--- src/pages/book.js
+++ src/pages/book.js
@@ -3,6 +3,11 @@ import Header from '../elements/header'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 import services from '../services/services'
+import bookObject from '../objects/book'
+import Grid from '@material-ui/core/Grid'
+import Rating from '@material-ui/lab/Rating'
+import Divider from '@material-ui/core/Divider'
+import { Link } from 'react-router-dom'
 
 const useStyles = makeStyles(theme => ({
   title: {
@@ -13,6 +18,7 @@ const useStyles = makeStyles(theme => ({
   bookList: {
     textAlign: 'center',
     marginTop: '1.75em',
+    marginLeft: '1.5em',
   },
 
   bookInfo: {
@@ -27,7 +33,12 @@ const useStyles = makeStyles(theme => ({
 
   image: {
     textAlign: 'left',
-    width: '20%',
+    width: '15%',
+  },
+
+  miniImage: {
+    textAlign: 'left',
+    width: '50%',
   },
 
   raiting: {
@@ -43,12 +54,16 @@ const useStyles = makeStyles(theme => ({
   buttons: {
     marginBottom: '1em',
   },
+
+  bought: {
+    paddingTop: '1em',
+  }
 }))
 
 export default function (props) {
   const classes = useStyles()
-  const [ book, setBook ] = useState({ title: "" })
-
+  const [ book, setBook ] = useState(bookObject)
+  
   useEffect(() => {
     services
       .getOne(props.id)
@@ -60,9 +75,74 @@ export default function (props) {
   return (
     <>
       <Header />
-      <Typography variant="h3" className={classes.title}>
-        {book.title}
+      <Grid
+        container
+        direction="row"
+        spacing={1}
+        className={classes.bookList}
+      >
+        <img
+          src={require('../images/under-the-dome-cover.jpeg')}
+          alt="cover_book"
+          className={classes.image}
+        />
+        <Grid item="contained">
+          <Grid 
+            container
+            direction="column"
+            className={classes.bookInfo}
+          >
+            <Typography variant="h3">
+              {book.title}
+            </Typography>
+            <Typography variant="subtitle1" color="textSecondary">
+              by {book.author}
+            </Typography>
+            <Typography variant="h4">
+              {book.price} ₽
+            </Typography>
+          </Grid>
+        </Grid>
+        <Rating name="read-only" value={book.rating} readOnly/>
+      </Grid>
+      <Divider variant="middle" className={classes.divider}/>
+      <Typography variant="h4" className={classes.bookInfo}>
+        Customers who bought this item also bought
       </Typography>
+      <Grid 
+        container
+        direction="row"
+        justify="center"
+        className={classes.bought}
+      >
+        {[1, 2, 3, 4, 5].map((number) => 
+        <Grid item xs={2}>
+          <Grid
+            container
+            direction="column"
+          >
+            <img
+              src={require('../images/under-the-dome-cover.jpeg')}
+              alt="cover"
+              className={classes.miniImage}
+            >
+            </img>
+            <Link to="/book/2">
+              <Typography variant="h6">
+                {book.title}
+              </Typography>
+            </Link>
+            <Typography variant="subtitle2">
+              {book.author}
+            </Typography>
+            <Rating name="read-only" value={book.rating}></Rating>
+            <Typography variant="h5">
+              {book.price} ₽
+            </Typography>
+          </Grid>
+        </Grid>
+        )}
+      </Grid>
     </>
   )
 }
